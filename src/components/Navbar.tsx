859158d25762990e270f0d0a057eb074c83b7020
Zyppon <contact.zyppon@gmail.com>
2023-06-09 02:16:07 +0200

    Added pages folder with DemoApp.tsx and updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,17 @@
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <>
+    <nav>
+      <img src=""></img>
+
+      <div className = "navlinks">
+        <li><a href="">Home</a></li>
+        <li><a href="">Github</a></li>
+        <li><a href="">Demo</a></li>
+      </div>
+    </nav>
+    </>
+  )
 }
 
 export default Navbar;
