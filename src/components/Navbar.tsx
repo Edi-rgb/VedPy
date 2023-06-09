55c31bcf2c14125269345ec7960a328a7692e7bb
Edi-rgb <dumitracheedi2@gmail.com>
2023-06-09 03:43:31 +0300

    edit link to github

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <>
-    <nav>
-      <ul>
-      <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/github">GitHub</Link>
-        </li>
-        <li>
-          <Link to="/demoapp">Demo App</Link>
-        </li>
-      </ul>
-    </nav>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="https://github.com/Edi-rgb/VedPy">Github</Link>
+          </li>
+          <li>
+            <Link to="/demoapp">Demo App</Link>
+          </li>
+        </ul>
+      </nav>
     </>
-  )
+  );
 }
 
 export default Navbar;
