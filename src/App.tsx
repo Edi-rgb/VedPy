2af4c9d741dd582b0e8c8d653b873366858dd1b2
Edi-rgb <dumitracheedi2@gmail.com>
2023-06-09 04:13:37 +0300

    added react router on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import Navbar from './components/Navbar'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
+import Navbar from "./components/Navbar";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  
-
   return (
     <>
-      <Navbar/>
-      <MainContent/>
-      <Footer/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<MainContent />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
